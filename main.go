ea4fb6c2a3984bac1d3ce22dd39d719693e05698
alissa-tung <alissa-tung@outlook.com>
2022-07-26 18:05:19 +0800

    dev: more logs

--- main.go
+++ main.go
@@ -1,21 +1,43 @@
 package main
 
-import "github.com/hstreamdb/hstreamdb-go-examples/examples"
+import (
+	"github.com/hstreamdb/hstreamdb-go-examples/examples"
+	"log"
+	"reflect"
+	"runtime"
+)
 
 func main() {
-	examples.ExampleCreateStream()
-	examples.ExampleListStreams()
+	xs := []func(){
+		examples.ExampleCreateStream,
+		examples.ExampleListStreams,
 
-	examples.ExampleWriteDataSimple()
-	examples.ExampleWriteDataBuffered()
-	examples.ExampleWriteDataWithKey()
+		examples.ExampleWriteDataSimple,
+		examples.ExampleWriteDataBuffered,
+		examples.ExampleWriteDataWithKey,
 
-	examples.ExampleCreateSubscription()
-	examples.ExampleListSubscriptions()
+		examples.ExampleCreateSubscription,
+		examples.ExampleListSubscriptions,
 
-	examples.ExampleConsumeDataSimple()
-	examples.ExampleConsumeDataShared()
+		examples.ExampleConsumeDataSimple,
+		examples.ExampleConsumeDataShared,
 
-	examples.ExampleDeleteSubscription()
-	examples.ExampleDeleteStream()
+		examples.ExampleDeleteSubscription,
+		examples.ExampleDeleteStream}
+
+	for _, x := range xs {
+		runFuncWithLog(x)
+	}
+
+}
+
+func getFuncName(i interface{}) string {
+	return runtime.FuncForPC(reflect.ValueOf(i).Pointer()).Name()
+}
+
+func runFuncWithLog(f func()) {
+	funcName := getFuncName(f)
+	log.Printf("start %s", funcName)
+	f()
+	log.Printf("end %s", funcName)
 }
